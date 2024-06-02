ccd77dde8faadbc20ae9515a44fb4aee89973fba
GiorgiabaZakhidze <giorgizabakhidze2011@gmail.com>
2024-06-03 01:02:41 +0400

    first commit

---
+++ day 04/homework.py
@@ -0,0 +1,33 @@
+# დავალება ნომერი1
+ball =  50 
+quantity =  6 
+
+print( ball * quantity )
+
+# დავალება ნომერი 2
+age =  12 
+age2 =  16 
+
+print( age + 10 )
+print( age2 + 10 )
+
+# დავალება ნომერი 3
+number1 = 20
+number2 = 25
+number3 = 30
+number4 = 40
+
+print( number1 *  number2)
+print( number3 +  number4)
+print( number4 -  number2)
+print( number4 /  number1)
+
+# დავალება ნომერი 4
+Name = ("giorgi ")
+age =   12
+print(Name + str(age))
+
+# დავალება ნომერი 5
+age1 = 12
+age2 = 36
+print(age2 - age1)
